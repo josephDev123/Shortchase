2192eeb996a20069f620058978818db88d52da65
unknown <josephuzuegbu55@gmail.com>
2023-06-22 20:52:28 +0100

    center the text in the landing button

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function login() {
   return (
     <div className='h-screen flex justify-center items-center w-4/5 mx-auto'>
-      <div className='flex flex-col gap-5 p-4 w-1/2'>
+      <div className='flex flex-col gap-5 p-4 w-3/4 basis-96'>
           <img src='https://raw.githubusercontent.com/josephDev123/Shortchase/master/client/src/assets/images/shortchase-logo.png' alt='' className='w-32 h-32 mt-6 rounded-full self-center' />
           <h3 className='font-bold text-xl'>Login to Shortchase</h3>
           <input type='email' className='border-2 p-3 rounded-xl w-full'></input>
